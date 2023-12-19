206906ee486cfd7ef86e7123fc8de7a6f8d225d9
Taiming <eefozeo@gmail.com>
2023-12-19 17:11:40 +0800

    feat: Add Slider component and implement results per page functionality

---
+++ src/components/Slider/index.tsx
@@ -0,0 +1,123 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+const SIZE_THUMB = 20;
+const step = 20;
+const defaultValue = 80;
+
+const railStyle = css`
+  background: rgba(255, 255, 255, 0.3); /* rail color */
+  width: 100%;
+  height: 6px;
+  border-radius: 5px;
+`;
+
+const trackStyle = css`
+  background: linear-gradient(to right, #ff5c01, #ffd05d); /* track color */
+  border-radius: 5px;
+  height: 6px;
+`;
+
+const StyledSlider = styled.input<{
+  $widthRatio: number;
+}>`
+  &[type='range'] {
+    -webkit-appearance: none;
+    outline: none;
+    position: relative;
+    z-index: 0;
+    ${railStyle}
+
+    &:before {
+      content: '';
+      position: absolute;
+      z-index: -1;
+      width: ${(props) => props.$widthRatio}%;
+      left: 0px;
+      ${trackStyle}
+    }
+  }
+
+  &[type='range']::-webkit-slider-thumb {
+    /* thumb style */
+    -webkit-appearance: none;
+    width: ${SIZE_THUMB}px;
+    height: ${SIZE_THUMB}px;
+    border-radius: 50%;
+    background: #000;
+    border: 5px solid #ffd05d;
+    cursor: pointer;
+    transition:
+      box-shadow 0.2s ease-in-out,
+      transform 0.1s ease-in-out;
+  }
+`;
+
+const DataList = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  padding-top: 16px;
+  box-sizing: border-box;
+  .datalist__item {
+    width: 20px;
+    text-align: center;
+  }
+`;
+
+const ListItem = styled.div<{
+  $active: boolean;
+}>`
+  color: ${(props) => (props.$active ? 'white' : 'rgba(255, 255, 255, 0.5)')};
+`;
+
+interface IProps {
+  min?: number;
+  max?: number;
+  value?: number;
+  options?: number[];
+  onChange?: (value: number) => void;
+}
+
+const Slider = (props: IProps) => {
+  const sliderRef = React.useRef(null);
+  const [currentValue, setCurrentValue] = React.useState(defaultValue);
+  const { onChange, max = 100, options = [3, 6, 9, 12, 15, 50] } = props;
+  const activeIndex = (currentValue + step) / step - 1;
+
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = Number(event?.target?.value);
+    setCurrentValue(value);
+  };
+
+  React.useEffect(() => {
+    if (onChange) {
+      onChange(activeIndex);
+    }
+  }, [currentValue, activeIndex, onChange]);
+
+  return (
+    <>
+      <StyledSlider
+        ref={sliderRef}
+        {...props}
+        type="range"
+        value={currentValue}
+        $widthRatio={(currentValue / max) * 100}
+        min={0}
+        max={100}
+        step={step}
+        onChange={handleOnChange}
+      />
+      <DataList>
+        {options.map((item, index) => (
+          <ListItem key={item} className="datalist__item" $active={index === activeIndex}>
+            {item}
+          </ListItem>
+        ))}
+      </DataList>
+    </>
+  );
+};
+
+export default Slider;
--- src/pages/Home/Search/index.tsx
+++ src/pages/Home/Search/index.tsx
@@ -1,11 +1,19 @@
+import React from 'react';
 import styled from 'styled-components';
 import Input from 'components/Input';
+import Slider from 'components/Slider';
+
+const options = [3, 6, 9, 12, 15, 50];
 
 const Divider = styled.div`
   width: 100%;
   height: 1px;
   background-color: var(--Greyscale-light, #fff);
   opacity: 0.1;
+  margin-top: 30px;
+  @media ${(props) => props.theme.mobile} {
+    display: none;
+  }
 `;
 
 const Container = styled.div`
@@ -35,10 +43,36 @@ const SearchSection = styled.div`
   display: flex;
   flex-direction: column;
   gap: 20px;
-  padding-bottom: 30px;
+`;
+
+const PageSizeSection = styled.div`
+  padding-top: 30px;
+  @media ${(props) => props.theme.mobile} {
+    padding-top: 28px;
+  }
+`;
+
+const Result = styled.div`
+  display: flex;
+  align-items: flex-end;
+  margin: 20px 0px;
+  .result__number {
+    font-size: 48px;
+    font-weight: 700;
+    height: 50px;
+  }
+  .result__unit {
+    font-size: 16px;
+    height: 24px;
+    letter-spacing: 0.15px;
+    margin-left: 10px;
+    display: flex;
+    align-items: flex-end;
+  }
 `;
 
 const Search = () => {
+  const [resultsPerPage, setResultsPerPage] = React.useState<number>();
   return (
     <Container>
       <Content>
@@ -46,6 +80,17 @@ const Search = () => {
           <Label>Search</Label>
           <Input placeholder="keyword" />
         </SearchSection>
+        <Divider />
+        <PageSizeSection>
+          <Label># of results per page</Label>
+          <Result>
+            <div className="result__number">{options[resultsPerPage || 0]}</div>
+            <div className="result__unit">results</div>
+          </Result>
+          <div>
+            <Slider options={options} onChange={(value) => setResultsPerPage(value)} />
+          </div>
+        </PageSizeSection>
         <Divider />
       </Content>
     </Container>
